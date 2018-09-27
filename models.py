56bf1712f404e1c1cf93bd7c3b61ab6844171b58
thinksource <foretribe@gmail.com>
2018-09-27 15:12:52 +1000

    additional

--- models.py
+++ models.py
@@ -9,10 +9,26 @@ class Category(db.Model):
     __tablename__ = 'Category'
     id = db.Column(postgresql.UUID(as_uuid=True), primary_key=True)
     name = db.Column(db.String(80), nullable=False)
+    category = relationship("Product", back_populates='category')
 
 class CategorySchema(ModelSchema):
     class Meta:
         model = Category
         sqla_session=db.session
 
+class Product(db.Model):
+    __tablename__='Product'
+    id = db.Column(postgresql.UUID, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    desc = db.Column(db.TEXT, nullable=True)
+    provider = db.Column(db.String(200), nullable=False)
+    barcode = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.DECIMAL(10, 2), nullable=False)
+    category_id=db.Column(postgresql.UUID, db.ForeignKey('Category.id'))
+    category = relationship("Category", back_populates='products')
+
+class ProductSchema(ModelSchema):
+    class Meta:
+        model = Product
+        sqla_session=db.session
 
